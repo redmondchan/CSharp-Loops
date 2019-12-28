d48a10d6279fab897a213cee126f606a2faacc46
Redmond Chan <redmondchan92@gmail.com>
2019-12-28 00:29:27 +0000

    added for loop with other arthimetic operators

--- main.cs
+++ main.cs
@@ -2,10 +2,18 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    
-    
-    for (int x = 1; x < 6; x++) {
-      Console.WriteLine($"Loop {x}");
+
+    //for loop with other arthimetic operators
+    for (int x = 10; x >= 0 ; x-=2) {
+      Console.WriteLine($"I have {x} days left to study.");
+      if (x == 0) {
+        Console.WriteLine("My test is today, I'm going to fail.");
+      };
     }
+    
+    // basic for loop
+    // for (int x = 1; x < 6; x++) {
+    //   Console.WriteLine($"Loop {x}");
+    // }
   }
 }
